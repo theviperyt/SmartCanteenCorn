67c47b46f57fdbf39cef274fa691161953dfe54a
tfxpanda0p <anderotechsb@gmail.com>
2026-03-21 00:23:32 +0530

    Logout log issue resolve

--- api/src/routers/userAuth.js
+++ api/src/routers/userAuth.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 
 const { register, login, logout } = require("../controllers/user");
 const { sensitiveLimiter } = require("../middleware/rateLimiter");
+const authUser = require("../middleware/authUser");
 
 router.post('/register', sensitiveLimiter, register);
 router.post('/login', sensitiveLimiter, login);
-router.post('/logout', logout);
+router.post('/logout', authUser, logout);
 
 module.exports = router;
